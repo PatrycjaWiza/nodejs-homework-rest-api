e7a85c7dce68420f63d3c34aedf1c891aa62c44e
PatrycjaWiza <95050810+PatrycjaWiza@users.noreply.github.com>
2022-12-01 21:55:58 +0100

    email

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join("public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", contactsRouter);
 
